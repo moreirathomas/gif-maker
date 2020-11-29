7e9dbde12c2a415a204072e71d320f916a9bd557
moreirathomas <moreirathomas97@gmail.com>
2020-11-29 16:59:40 +0100

    version 1 running

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,139 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './fontello/css/fontello-embedded.css';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+const ffmpeg = createFFmpeg({ log: true });
 
 function App() {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
+  const [ready, setReady] = useState(false);
+  const [video, setVideo] = useState();
+  const [gif, setGif] = useState();
+  const [converting, setconverting] = useState(false);
+  const [outFilename, setOutFilename] = useState();
+
+  const load = async () => {
+    await ffmpeg.load();
+    setReady(true);
+  };
+
+  const convertToGif = async () => {
+    setconverting(true);
+
+    ffmpeg.FS('writeFile', 'in.mp4', await fetchFile(video));
+
+    await ffmpeg.run('-i', 'in.mp4', '-t', '2.5', '-ss', '2.0', '-f', 'gif', 'out.gif'); // prettier-ignore
+
+    const data = ffmpeg.FS('readFile', 'out.gif');
+
+    const url = URL.createObjectURL(
+      new Blob([data.buffer], { type: 'image/gif' }),
+    );
+
+    setconverting(false);
+    setGif(url);
+    setOutFilename(video.name.replace('mp4', 'gif'));
+  };
+
   useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    load();
+  }, []);
+
+  const AppHeader = () => {
+    return (
+      <header>
+        <h1>gifmakr</h1>
+        <div>
+          <p className="lead">Upload a video file and convert it to gif.</p>
+          <p className="links">
+            <a
+              href="https://github.com/moreirathomas"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="icon-github-circled"></i>
+            </a>
+          </p>
+        </div>
+      </header>
+    );
+  };
+
+  const AppFooter = () => {
+    return (
+      <footer>
         <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
+          Made with{' '}
           <a
-            className="App-link"
-            href="https://reactjs.org"
+            href="http://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            React
+          </a>{' '}
+          and{' '}
+          <a
+            href="https://ffmpeg.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            FFmpeg
           </a>
+          .
         </p>
-      </header>
+      </footer>
+    );
+  };
+
+  return ready ? (
+    <div className="App">
+      <AppHeader />
+
+      <main>
+        <div className="input-container">
+          <h2>Video input</h2>
+
+          <input
+            type="file"
+            aria-label="Upload video"
+            className="custom-file-input"
+            onChange={(e) => setVideo(e.target.files?.item(0))}
+          />
+
+          {video && (
+            <div className="input-video">
+              <pre>{video.name}</pre>
+              <video controls src={URL.createObjectURL(video)}></video>
+            </div>
+          )}
+        </div>
+
+        <div className="output-container">
+          <h2>Gif output</h2>
+
+          {!gif && (
+            <button disabled={!video} onClick={convertToGif}>
+              Convert
+            </button>
+          )}
+
+          {converting && <pre>Convertion in progress...</pre>}
+
+          {gif && (
+            <div className="output-gif">
+              <a href={gif} download={outFilename}>
+                Download
+              </a>
+              <pre>{outFilename}</pre>
+              <img src={gif} />
+            </div>
+          )}
+        </div>
+      </main>
+
+      <AppFooter />
     </div>
+  ) : (
+    <div className="App loading">Loading...</div>
   );
 }
 
